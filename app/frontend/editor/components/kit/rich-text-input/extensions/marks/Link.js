0da5a72c51c4b78cc666b54c8e735560b9dda687
Ron B <35236981+ronb54@users.noreply.github.com>
2025-08-03 19:29:51 -0700

    Update extensions/marks/Link.js
    Signed-off-by: Ron B <35236981+ronb54@users.noreply.github.com>

--- app/frontend/editor/components/kit/rich-text-input/extensions/marks/Link.js
+++ app/frontend/editor/components/kit/rich-text-input/extensions/marks/Link.js
@@ -1,57 +1,58 @@
-import { Link } from 'tiptap-extensions'
+import { Link } from '@tiptap/extension-link'
 
-export default class MaglevLink extends Link {
-  get name() {
-    return 'link'
-  }
-
-  get schema() {
+const MaglevLink = Link.extend({
+  inclusive() {
+    return false;
+  },
+  addAttributes() {
     return {
-      attrs: {
-        href: {
-          default: null,
-        },
-        target: {
-          default: null,
-        },
-        linkType: {
-          default: null,
-        },
-        linkId: {
-          default: null,
-        },
-        sectionId: {
-          default: null,
-        },
+      href: {
+        default: null,
       },
-      inclusive: false,
-      parseDOM: [
-        {
-          tag: 'a[href]',
-          getAttrs: (dom) => ({
-            href: dom.getAttribute('href'),
-            target: dom.getAttribute('target'),
-            linkType: dom.getAttribute('maglev-link-type'),
-            linkId: dom.getAttribute('maglev-link-id'),
-            sectionId: dom.getAttribute('maglev-section-id'),
-          }),
-        },
-      ],
-      toDOM: (node) => {
-        const { linkType, linkId, sectionId, ...attrs } = node.attrs
-        return [
-          'a',
-          {
-            ...attrs,
-            rel: 'noopener noreferrer nofollow',
-            target: attrs.target || this.options.target,
-            'maglev-link-type': linkType,
-            'maglev-link-id': linkId,
-            'maglev-section-id': sectionId,
-          },
-          0,
-        ]
+      target: {
+        default: null,
+      },
+      linkType: {
+        default: null,
+      },
+      linkId: {
+        default: null,
+      },
+      sectionId: {
+        default: null,
       },
     }
-  }
-}
+  },
+  parseHTML() {
+    return [
+      {
+        tag: 'a[href]',
+        getAttrs: (dom) => ({
+          href: dom.getAttribute('href'),
+          target: dom.getAttribute('target'),
+          linkType: dom.getAttribute('maglev-link-type'),
+          linkId: dom.getAttribute('maglev-link-id'),
+          sectionId: dom.getAttribute('maglev-section-id'),
+        }),
+      },
+    ]
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    const { linkType, linkId, sectionId, ...attrs } = HTMLAttributes
+    return [
+      'a',
+      {
+        ...attrs,
+        rel: 'noopener noreferrer nofollow',
+        target: attrs.target || this.options.target,
+        'maglev-link-type': linkType,
+        'maglev-link-id': linkId,
+        'maglev-section-id': sectionId,
+      },
+      0,
+    ]
+  },
+});
+
+export default MaglevLink;
